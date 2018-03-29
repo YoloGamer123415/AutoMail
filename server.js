07315758b3f7ea31ff9df6a9c13cb69648ff9f76
YoloGamer123415 <36742291+YoloGamer123415@users.noreply.github.com>
2018-03-29 11:04:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ io.on("connection", function(socket) {
   socket.on("sendMail", sendMail); //of function(){sendMail} maar dit is cleaner
 });
 
-http.listen(8000, function () {
-  console.log("listening on *:8000");
+http.listen(80, function () {
+  console.log("listening on *:80");
 });
 
 function sendMail(data) {
@@ -32,20 +32,32 @@ function sendMail(data) {
   const to = data.to
   const subject = data.subject
   const text = data.text
+  const timesToSend = data.timesToLoop
+
+  var timesSend = 0;
 
   const mailOptions = {
     from: "automatic.bot.001@gmail.com",
     to: to,
     subject: subject,
-    html: text + `<br /><center><h6>This mail has been sent automatically. Replaying to this mail doesn't work.</h6> <a href='https://molmassa.000webhostapp.com'>Our website</a></center>`
+    html: "<style> .lowerText {color: rgb(0, 0, 0);} .website {color: rgb(0, 0, 0); text-decoration: none;} </style> <div class='mail'> <div class='text'>" + text + "</div> <br /><br /> <center> <h6 class='lowerText'>This mail has been sent automatically. Replaying to this mail doesn't work.</h6> <h6> <a class='website' href='https://molmassa.000webhostapp.com'>Our website</a> </h6> </center> </div>"
   };
 
+  var timer = setInterval(function(){
+    if(timesSend < timesToSend){
+      timesSend++;
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email '" + subject + "' has been sent " + timesLooped + " time(s) to '" + to + "': " + info.response);
+        }
+      });
     } else {
-      console.log("Email sent: " + info.response);
+      clearInterval(timer);
+      console.log("All mails have been send.")
     }
-  });
+  }, 2000)
+
 }
